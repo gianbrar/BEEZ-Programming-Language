0c2ccfc4a938b89ed425b282159fe59e706f7fa7
mmmmmk, here's hiddenmonkey <gian.s.brar@gmail.com>
2020-03-23 02:26:55 +0000

    Even more function functionality

--- src/function.hpp
+++ src/function.hpp
@@ -1,9 +1,11 @@
+#define FUNC_NOT
 #include "libraries.hpp"
 
 class function {
   public:
     std::string args[];
     std::string name;
+    std::string contents;
     function(std::string arrayFuncName, std::string givenArgs[]) {
       for (int i = 0; i < givenArgs.size(); i++) {
         args[i] = givenArgs[i];
