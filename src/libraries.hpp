cf876136ed751a573f6e99c422054fae971e7295
thehiddenmonkey <gian.s.brar@gmail.com>
2020-03-23 14:01:20 -0400

    Bugfix

--- src/libraries.hpp
+++ src/libraries.hpp
@@ -1,6 +1,6 @@
 #include <fstream>
-#include <boost/algorithim/string.hpp>
 #include <boost/algorithm/string/find.hpp>
+#include <boost/algorithm/string.hpp>
 #include <iostream>
 #include <string>
 #ifdef FUNC_NOT
