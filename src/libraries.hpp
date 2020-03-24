5c3ed03f97c1ade0d52acffe9a6dfb86771d076b
mmmmmk, here's hiddenmonkey <gian.s.brar@gmail.com>
2020-03-24 20:45:23 +0000

    Deleted src; moved to b++

--- src/libraries.hpp
+++
@@ -1,8 +0,0 @@
-#include <fstream>
-#include <boost/algorithm/string/find.hpp>
-#include <boost/algorithm/string.hpp>
-#include <iostream>
-#include <vector>
-#include <iterator>
-#include <cstddef>
-#include <string>
