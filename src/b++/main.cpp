378fb617605d07433a9f1a474ca2f927b065260c
thehiddenmonkey <gian.s.brar@gmail.com>
2020-04-02 21:16:31 -0400

    Mega Updategit push!

--- src/b++/main.cpp
+++ src/b++/main.cpp
@@ -7,7 +7,7 @@ bool hive = false;
 bool inFunction = false;
 bool debugMode = false;
 int combMax;
-std::string interpret;
+std::string fValue;
 std::string hiveType;
 std::vector<function> vFunc;
 function uFunc;
@@ -17,6 +17,28 @@ char endingChar;
 int iStartingChar;
 int currentFuncDef = -1;
 int maxFuncDef;
+bool possibleJoke = false;
+bool commandRecognized = false;
+std::vector<std::string> stripes;
+bool firstWingDefined = false;
+bool firstWhile = true;
+bool commentJoke = false;
+bool spaceJoke = false;
+int spaceI = 0;
+int commentI = 0;
+int currentIteration = 0;
+int lineCount = 0;
+std::string empty;
+std::string firstWingContent;
+int stripeCount = 0;
+std::string fileName;
+std::string secondary;
+std::string secondWingContent;
+bool connectionStart = false;
+bool bodyDefined = false;
+std::string tempVariable;
+bool possibleCommand = false;
+std::vector<variable> varList;
 
 int debug(std::string msg) {
   if (debugMode == true) {
@@ -71,7 +93,7 @@ int charCheck() {
 
 int createHive() {
   hive = true;
-  hiveType = interpret.substr(1, interpret.length() - 1);
+  hiveType = fValue.substr(1, fValue.length() - 1);
   if (hiveType.substr(0, 3) != "CELL") {
     std::string CSHiveType = removeCS(removeCS(hiveType, 'S'), 'C');
     std::vector<int> hiveTypeContent;
@@ -133,75 +155,9 @@ int createHive() {
   return 0;
 }
 
-int main(int argc, char** argv) {
-  std::string fileName;
-  std::string secondary;
-  bool connectionStart = false;
-  bool possibleCommand = false;
-  if (argc > 1) {
-      fileName = argv[1];
-      if (argc > 2) {
-        secondary = argv[2];
-        if (secondary == "-d" || secondary == "--debug") {
-          debugMode = true;
-        }
-      }
-    }
-  else {
-      ERR "No input files given." ENDL;
-      return 0;
-  }
-  std::string argv1;
-  std::stringstream ssArgv1;
-  ssArgv1 << argv[1];
-  ssArgv1 >> argv1;
-  if (argv1.at(0) == '-') {
-    possibleCommand = true;
-    if (fileName == "-m" || fileName == "--man") {
-      std::cout << "Welcome to b++, an interpreter created for the BEEZ Programming Language.\nGENERAL STRUCTURE OF COMMAND:\nb++ {file name or optional command here} {secondary optional command here}\nOPTIONAL COMMANDS:\nb++ -m or b++ --man: Brings up this help page.\nb++ {file name} -d or b++ {file name} --debug: Runs file in debug mode. (for compiler maintainers)" ENDL;
-      return 0;
-    }
-    else if (fileName == "-d" || fileName == "--debug") {
-      ERR "Debug mode cannot be activated without input file." ENDL;
-      return 0;
-    }
-  }
-  if (boost::algorithm::ends_with(fileName, ".BUZZ") == false) {
-    if (boost::algorithm::iends_with(fileName, ".BUZZ") == true) {
-      fileName.erase(fileName.length() - 5, fileName.length());
-    }
-    fileName += ".BUZZ";
-  }
-  std::ifstream buzzFile(fileName.c_str());
-  if (!buzzFile) {
-    ERR "File '" << fileName << "' does not exist!" ENDL;
-    if (possibleCommand == true) {
-      std::cout << "Did you mean to type in a command?" ENDL;
-    }
-    return 0;
-  }
-  if (buzzFile.peek() == std::ifstream::traits_type::eof()) {
-    ERR "File is blank." ENDL;
-    return 0;
-  }
-  bool firstWhile = true;
-  bool commentJoke = false;
-  bool spaceJoke = false;
-  int spaceI = 0;
-  int commentI = 0;
-  int currentIteration = 0;
-  buzzFile.open(fileName.c_str());
-  int lineCount = 0;
-  std::string empty;
-  while (getline(buzzFile, empty)) {
-    lineCount++;
-  }
-  buzzFile.close();
-  buzzFile.open(fileName.c_str());
-  bool possibleJoke = false;
-  bool commandRecognized = false;
-  while (getline(buzzFile, interpret)) {
-    if (debugMode == true) {
+int process(std::string interpret) {
+  interpret = removeCS(interpret, 'C');
+  if (debugMode == true) {
     	if (firstWhile == true) {
 	      std::cout << "Began while loop" ENDL;
     	}
@@ -216,7 +172,7 @@ int main(int argc, char** argv) {
        if (debugMode == true) {
 	       std::cout << "Skipping current loop" ENDL;
        }
-       continue;
+       return 0;
     }
     std::string varCheck = removeCS(interpret, 'C');
     if (debugMode == true) {
@@ -230,19 +186,15 @@ int main(int argc, char** argv) {
       commentI += 1;
     }
     varCheck = removeCS(varCheck, 'S');
-    if (debugMode == true) {
-	    std::cout << "Successfully removed all spsaces." ENDL;
-    }
+	  debug("Successfully removed all spaces.");
     if (varCheck == "") {
       if (commentJoke == false) {
-	      if (debugMode == true) {
-	        std::cout << "Possible space joke trigger detected." ENDL;
-	      }
+	      debug("Possible space joke trigger detected.");
         spaceJoke = true;
         spaceI += 1;
       }
       possibleJoke = true;
-      continue;
+      return 0;
     }
     if (varCheck.at(0) == '^') {
       commandRecognized = true;
@@ -262,6 +214,36 @@ int main(int argc, char** argv) {
         ERR "Hive type already declared." ENDL;
         return 0;
       }
+      return 0;
+    }
+    if (interpret.find("BUZZ@") != std::string::npos) {
+      debug("Detecting BUZZ@");
+      interpret.erase(0, 5);
+      if (interpret.substr(0, 7) == "console") {
+        debug("Sending message to console");
+        interpret.erase(0, 8);
+        varCheck.erase(0, 12);
+        if (varCheck.at(0) == '"') {
+          debug("Found string");
+          int firstQuotation = interpret.find('"');
+          std::string secondQuotation = interpret.substr(interpret.find('"') + 1, interpret.length() - 1);
+          interpret = interpret.substr(firstQuotation + 1, secondQuotation.find('"'));
+          while (interpret.find("</") != std::string::npos && interpret.substr(interpret.find("</"), interpret.find("</") - 1) != "\\") {
+            tempVariable = interpret.substr(interpret.find("</") + 2, interpret.length() - 1);
+            tempVariable = tempVariable.substr(0, tempVariable.find(" ") - 1);
+            for (int i = 0; i < varList.size(); i++) {
+              if (varList.at(i).name == tempVariable) {
+                interpret.replace(interpret.find("</") + 2, tempVariable.length(), varList.at(i).contents);
+              }
+            }
+          }
+        }
+        else {
+          ERR "No string detected." ENDL;
+        }
+      }
+      std::cout << interpret << std::endl;
+      return 0;
     }
     if (varCheck.substr(0, 3) == "BUZZ") {
       commandRecognized = true;
@@ -321,22 +303,138 @@ int main(int argc, char** argv) {
           WARN "Function declared without curly brackets." ENDL;
         }  
       }
-      continue;
+      return 0;
     }
   }
+  if (interpret.find("}") != std::string::npos) {
+    if (inFunction) {
+      inFunction = false;
+    }
+    else {
+      WARN "Ignoring '}' character." ENDL;
+    }
+  }
+  if (varCheck.find(":(") != std::string::npos) {
+    debug("Inside bee body");
+    if (!connectionStart) {
+      ERR "Cannot start connection without wings." ENDL;
+      return 0;
+    }
+    if (interpret.find("|") == std::string::npos) {
+      ERR "No stripes found. Bee is entirely yellow and currently facing an existential crisis." ENDL;
+      return 0;
+    }
+    bodyDefined = true;
+    while (interpret.find("|") != std::string::npos) {
+      ++stripeCount;
+      interpret = interpret.substr(interpret.find("|") + 1, interpret.length() - 1);
+      if (interpret.find("|") != std::string::npos) {
+        stripes.push_back(interpret.substr(0, interpret.find("|") - 1));
+      }
+      else if (stripeCount < 2) {
+        ERR "At least two stripes must be given." ENDL;
+      }
+    }
+    return 0;
+  }
   if (interpret.find("(") != std::string::npos) {
-    if (interpret.substr(interpret.find("("), interpret.find("(") + 1) != ")") {
-      if (connectionStart == true) {
+    if (interpret.find(")") != std::string::npos) {
+      if (connectionStart && !bodyDefined) {
         ERR "Cannot define connection inside connection. You just smashed two bees into each other. You monster." ENDL;
         return 0;
       }
-      connectionStart = true;
-      continue;
+      else if (!connectionStart && !bodyDefined) {
+        debug("Defining first wings");
+        if (varCheck.substr(varCheck.find("(") + 1) != ")") {
+          firstWingContent = varCheck.substr(varCheck.find("(") + 1, varCheck.find(")") - 1);
+        }
+        else {
+          firstWingContent = "BLANK";
+        }
+        connectionStart = true;
+        return 0;
+      }
+      else if (connectionStart && bodyDefined) {
+        debug("Defining second wings");
+        secondWingContent = varCheck.substr(varCheck.find("(") + 1, varCheck.find(")") - 1);
+        for (int i = 0; i < stripes.size(); i++) {
+          debug("Running process with stripes instead of fValue.");
+          process(stripes.at(i));
+        }
+        connectionStart = false;
+        bodyDefined = false;
+      }
+      else {
+        ERR "This literally shouldn't be possible, but why not put an error here just for debugging purposes? Debeeing, one could even say!\n...comedy is not an interpreter's specialty." ENDL;
+      }
+    }
+    else {
+        ERR "No closing paranthese found!" ENDL;
+        return 0;
+    }  
+  }
+  return 0;
+}
+
+int main(int argc, char** argv) {
+  if (argc > 1) {
+      fileName = argv[1];
+      if (argc > 2) {
+        secondary = argv[2];
+        if (secondary == "-d" || secondary == "--debug") {
+          debugMode = true;
+        }
+      }
+    }
+  else {
+      ERR "No input files given." ENDL;
+      return 0;
+  }
+  std::string argv1;
+  std::stringstream ssArgv1;
+  ssArgv1 << argv[1];
+  ssArgv1 >> argv1;
+  if (argv1.at(0) == '-') {
+    possibleCommand = true;
+    if (fileName == "-m" || fileName == "--man") {
+      std::cout << "Welcome to b++, an interpreter created for the BEEZ Programming Language.\nGENERAL STRUCTURE OF COMMAND:\nb++ {file name or optional command here} {secondary optional command here}\nOPTIONAL COMMANDS:\nb++ -m or b++ --man: Brings up this help page.\nb++ {file name} -d or b++ {file name} --debug: Runs file in debug mode. (for compiler maintainers)" ENDL;
+      return 0;
+    }
+    else if (fileName == "-d" || fileName == "--debug") {
+      ERR "Debug mode cannot be activated without input file." ENDL;
+      return 0;
     }
   }
-  else {
-    ERR "No closing paranthese found!" ENDL;
+  if (boost::algorithm::ends_with(fileName, ".BUZZ") == false) {
+    if (boost::algorithm::iends_with(fileName, ".BUZZ") == true) {
+      fileName.erase(fileName.length() - 5, fileName.length());
+    }
+    fileName += ".BUZZ";
   }
+  std::ifstream buzzFile(fileName.c_str());
+  if (!buzzFile) {
+    ERR "File '" << fileName << "' does not exist!" ENDL;
+    if (possibleCommand == true) {
+      std::cout << "Did you mean to type in a command?" ENDL;
+    }
+    return 0;
+  }
+  if (buzzFile.peek() == std::ifstream::traits_type::eof()) {
+    ERR "File is blank." ENDL;
+    return 0;
+  }
+  buzzFile.open(fileName.c_str());
+  while (getline(buzzFile, empty)) {
+    lineCount++;
+  }
+  buzzFile.close();
+  buzzFile.open(fileName.c_str());
+  bool possibleJoke = false;
+  bool commandRecognized = false;
+  std::vector<std::string> stripes;
+  bool firstWingDefined = false;
+  while (getline(buzzFile, fValue)) {
+    process(fValue);
   }
   buzzFile.close();
   std::string jokeTest;
